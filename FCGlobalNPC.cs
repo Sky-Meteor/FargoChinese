e2093f96e87cd575f74c3095087394d0ac990e03
FallinLeaf <3324888485@qq.com>
2022-08-25 20:39:38 +0800

    new chats for latest fargowiltas

--- FCGlobalNPC.cs
+++ FCGlobalNPC.cs
@@ -106,6 +106,8 @@ namespace FargoChinese
             }
             if (chat == "UWAH! Please don't hurt... wait, it's just you. Don't scare me like that! And why is that THING following you?!")
                 chat = "呜哇！别伤害我……等等，是你啊……不要这样吓我！还有，为什么那东西在跟着你？！";
+            if (chat == "Ooh, that's my hoodie! So how is it? Comfy and great for gaming, right? Maybe you'll even go beat a boss without taking damage!")
+                chat = "哦，那是我的连帽衫！所以感觉如何？舒适、很适合打游戏，对吧？也许你甚至会去无伤击败一个Boss！";
             if (chat == "Good work getting one over on me! Hope I didn't make you sweat too much. Keep at the grind - I wanna see how far you can go!")
                 chat = "不错，你战胜了我！希望我没有让你太辛苦。坚持到底吧——我想要看看你能前行多远！";
             if (chat == "The blood moon's effects? I'm not human anymore, so nope!")
