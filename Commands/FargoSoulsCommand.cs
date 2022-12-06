03b7ef050516cf2c1635c1b52ac7b3b4cb93813d
Sky-Meteor <2746501855@qq.com>
2022-12-06 18:14:55 +0800

    WHAT IS THIS?

--- Commands/FargoSoulsCommand.cs
+++ Commands/FargoSoulsCommand.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Xna.Framework;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
 using FargowiltasSouls.Items.Accessories.Enchantments;
@@ -18,6 +19,8 @@ using FargowiltasSouls.Items.Weapons.SwarmDrops;
 using FargowiltasSouls.Patreon.Catsounds;
 using FargowiltasSouls.Patreon.Volknet;
 using FargowiltasSouls.Items.Accessories.Masomode;
+using FargowiltasSouls.Toggler;
+using Terraria.Localization;
 
 namespace FargoChinese.Commands
 {
@@ -30,8 +33,34 @@ namespace FargoChinese.Commands
 
         public override string Command => "souls";
 
+        public override string Usage =>
+@"用法：/souls [关键词]
+关键词列表：
+ench, enchantment, 魔石 -> 查看所有魔石
+essence, 精华 -> 查看所有精华
+force, 力 -> 查看所有力
+eternity, maso, masomode, 永恒, 永恒饰品 -> 查看所有永恒饰品
+soul, 魂 -> 查看所有魂
+armor, 盔甲, 装备 -> 查看所有盔甲
+summon, summoner, 召唤物 -> 查看所有Fargo魂添加的召唤物
+weapon, 武器 -> 查看所有武器
+patreon, 捐赠者物品, 捐赠者 -> 查看所有捐赠者物品
+log, changelog, 日志, 更新日志 -> 查看最新的更新日志（自动打开网页）
+t -> 使用预设饰品效果（用法：/souls t [关键词1] [关键词2]）";
+
+        public override string Description => "这是与Fargo魂有关的一些实用指令";
+
+        Dictionary<string, bool> Toggles;
+
+        public override void Load()
+        {
+            Toggles = new Dictionary<string, bool>();
+        }
+
         public override void Action(CommandCaller caller, string input, string[] args)
         {
+            Player player = caller.Player;
+
             if (args.Length > 0)
             {
                 switch (args[0])
@@ -66,6 +95,7 @@ namespace FargoChinese.Commands
                         PrintItem(ItemType<EridanusBattleplate>(), ItemType<StyxLeggings>());
                         break;
                     case "summon":
+                    case "summoner":
                     case "召唤物":
                         PrintItem(ItemType<AbominationnVoodooDoll>(), ItemType<SquirrelCoatofArms>());
                         break;
@@ -84,7 +114,89 @@ namespace FargoChinese.Commands
                     case "更新日志":
                         Utils.OpenToURL("https://www.bilibili.com/read/cv20213491");
                         break;
+                    case "t":
+                        if (args.Length > 1)
+                        {
+                            string[] _args = input[9..].Split(" ");
+                            foreach (string arg in _args)
+                            {
+                                switch (arg)
+                                {
+                                    case "nodash":
+                                        NoDash:
+                                        Toggles.Merge(new Dictionary<string, bool>() 
+                                        {
+                                            { "JungleDash", false },
+                                            { "Monk", false },
+                                            { "ShinobiDash", false },
+                                            { "Shinobi", false },
+                                            { "CrystalDash", false },
+                                            { "Solar", false },
+                                            { "SupersonicTabi", false },
+                                            { "CthulhuShield", false },
+                                            { "DeerSinewDash", false },
+                                            { "MasoSkeleSpin", false },
+                                        });
+                                        continue;
+                                    case "[i:58]Furgo[i:58]力[i:58]荐[i:58]":
+                                        for (int i = 0; i < 200; i++)
+                                            CombatText.NewText(new Rectangle((int)player.position.X + Main.rand.Next(-1000, 1000), (int)player.position.Y + Main.rand.Next(-500, 500), 1, 1), Color.Red, Main.rand.Next(new string[] { "¿", "?", "¡", "!" }));
+                                        goto NoDash;
+                                    default:
+                                        Main.NewText($"参数{arg}输入错误", Color.Red);
+                                        continue;
+                                }
+                            }
+                            string nameCollectionT = default;
+                            string nameCollectionF = default;
+                            int numT = 0;
+                            int numF = 0;
+                            foreach (KeyValuePair<string, bool> pair in Toggles)
+                            {
+                                if (numT > 9)
+                                {
+                                    numT = 0;
+                                    Main.NewText($"{nameCollectionT[..(nameCollectionT.Length - 1)]} 已开启");
+                                    nameCollectionT = default;
+                                }
+                                if (numF > 9)
+                                {
+                                    numF = 0;
+                                    Main.NewText($"{nameCollectionF[..(nameCollectionF.Length - 1)]} 已关闭");
+                                    nameCollectionF = default;
+                                }
+
+                                string name = Language.GetTextValue($"Mods.FargowiltasSouls.Toggler.{pair.Key}Config");
+                                foreach (KeyValuePair<string, string> enchColor in FCGlobalItem.enchColors)
+                                {
+                                    if ($"{pair.Key}Config" == enchColor.Key)
+                                    {
+                                        name = $"[c/{enchColor.Value}:{name}]";
+                                        break;
+                                    }
+                                }
+                                if (pair.Value == true)
+                                {
+                                    numT += 1;
+                                    nameCollectionT += name + "，";
+                                }
+                                else
+                                {
+                                    numF += 1;
+                                    nameCollectionF += name + "，";
+                                }
+                                player.SetToggleValue(pair.Key, pair.Value);
+                            }
+                            if (!string.IsNullOrEmpty(nameCollectionT))
+                                Main.NewText($"{nameCollectionT[..(nameCollectionT.Length - 1)]} 已开启");
+                            if (!string.IsNullOrEmpty(nameCollectionF))
+                                Main.NewText($"{nameCollectionF[..(nameCollectionF.Length - 1)]} 已关闭");
+                        }
+                        break;
+                    case "test":
+                        break;
                     default:
+                        Main.NewText($"参数{args[0]}输入错误", Color.Red);
                         break;
                 }
 
@@ -102,6 +214,11 @@ namespace FargoChinese.Commands
             /*foreach (string i in args)
                 Main.NewText(i);
             CombatText.NewText(caller.Player.Hitbox, Main.DiscoColor, "yee");*/
+        }
+
+        public override void Unload()
+        {
+            Toggles = null;
         }
     }
 }
---
+++ FCUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FargoChinese
+{
+    public static class FCUtils
+    {
+        public static void Merge<TKey, TValue>(this Dictionary<TKey, TValue> Dic1, Dictionary<TKey, TValue> Dic2)
+        {
+            if (Dic1 == null || Dic2 == null)
+                return;
+            foreach (KeyValuePair<TKey, TValue> item in Dic2)
+            {
+                if (Dic1.ContainsKey(item.Key))
+                    Dic1.Remove(item.Key);
+                Dic1.Add(item.Key, item.Value);
+            }
+        }
+    }
+}
