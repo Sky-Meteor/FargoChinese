2a548c8d53c7b8bd42f6a94bfd25fab201a1a489
Sky-Meteor <2746501855@qq.com>
2022-12-06 18:35:57 +0800

    remove unused usings

--- FCGlobalItem.cs
+++ FCGlobalItem.cs
@@ -2,10 +2,6 @@
 using Fargowiltas.Items.CaughtNPCs;
 using Fargowiltas.Items.Summons;
 using Fargowiltas.NPCs;
-using FargowiltasSouls.Items.Accessories.Enchantments;
-using FargowiltasSouls.Items.Accessories.Forces;
-using FargowiltasSouls.Utilities;
-using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
