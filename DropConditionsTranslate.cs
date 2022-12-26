120c269daf42c76e4fb7e7d5f0ddc13a10820490
Sky-Meteor <2746501855@qq.com>
2022-12-26 15:50:00 +0800

    update .4

--- DropConditionsTranslate.cs
+++ DropConditionsTranslate.cs
@@ -34,9 +34,16 @@ namespace FargoChinese
                     string conditionDescription = condition.GetConditionDescription();
                     if (!string.IsNullOrWhiteSpace(conditionDescription))
                     {
-                        if (conditionDescription == "This is a Master/Eternity Mode drop rate")
-                            conditionDescription = "这是大师或永恒模式下的掉落率";
-                        list.Add(conditionDescription);
+                        list.Add(conditionDescription
+                            .Replace("Hardmode Eternity Mode drop rate", "在困难模式下的永恒模式下的的掉落率")
+                            .Replace("Pre-Hardmode Eternity Mode drop rate", "在困难模式之前的永恒模式下的掉落率")
+                            .Replace("This is a Master/Eternity Mode drop rate", "这是大师或永恒模式下的掉落率")
+                            .Replace("Non-Eternity Mode drop rate", "在非永恒模式下的掉落率")
+                            .Replace("Eternity Mode drop rate", "在永恒模式下的掉落率")
+                            .Replace("捐赠者掉落 在永恒模式", "在永恒模式下作为捐赠者物品的掉落率")
+                            .Replace("捐赠者掉落", "作为捐赠者物品的掉落率")
+                            .Replace("在血月的丛林", "（在血月期间的丛林）")
+                            .Replace("Tim's Concoction drop rate", "佩戴蒂姆的秘药时的掉落率"));
                     }
                 }
             }
