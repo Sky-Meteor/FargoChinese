3721d8b4dc916c84931f7a09c206aef1039e939b
Sky-Meteor <2746501855@qq.com>
2023-02-19 15:39:03 +0800

    optimization s∞n

--- Patch/FargowiltasSouls/DropConditionsTranslate.cs
+++ Patch/FargowiltasSouls/DropConditionsTranslate.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
 using Terraria;
 using Terraria.GameContent.ItemDropRules;
 using Terraria.ModLoader;
@@ -13,46 +16,35 @@ namespace FargoChinese.Patch.FargowiltasSouls
 
         public override void Load()
         {
-            On.Terraria.GameContent.UI.Elements.UIBestiaryInfoItemLine.SetBestiaryNotesOnItemCache += UIBestiaryInfoItemLine_SetBestiaryNotesOnItemCache;
+            IL.Terraria.GameContent.UI.Elements.UIBestiaryInfoItemLine.SetBestiaryNotesOnItemCache += UIBestiaryInfoItemLine_SetBestiaryNotesOnItemCache;
         }
 
-        private static void UIBestiaryInfoItemLine_SetBestiaryNotesOnItemCache(On.Terraria.GameContent.UI.Elements.UIBestiaryInfoItemLine.orig_SetBestiaryNotesOnItemCache orig, Terraria.GameContent.UI.Elements.UIBestiaryInfoItemLine self, DropRateInfo info)
+        private static void UIBestiaryInfoItemLine_SetBestiaryNotesOnItemCache(ILContext il)
         {
-            List<string> list = new List<string>();
-            if (info.conditions == null)
-            {
+            var c = new ILCursor(il);
+
+            if (!c.TryGotoNext(i => i.MatchCallvirt(typeof(List<string>).GetMethod("Add"))))
                 return;
-            }
-
-            foreach (IItemDropRuleCondition condition in info.conditions)
+            Replace("Pre-Hardmode Eternity Mode drop rate", "在困难模式之前的永恒模式下的掉落率");
+            Replace("Hardmode Eternity Mode drop rate", "在困难模式下的永恒模式下的的掉落率");
+            Replace("This is a Master/Eternity Mode drop rate", "这是大师或永恒模式下的掉落率");
+            Replace("Non-Eternity Mode drop rate", "在非永恒模式下的掉落率");
+            Replace("Eternity Mode drop rate", "在永恒模式下的掉落率");
+            Replace("捐赠者掉落 在永恒模式", "在永恒模式下作为捐赠者物品的掉落率");
+            Replace("捐赠者掉落", "作为捐赠者物品的掉落率");
+            Replace("在血月的丛林", "（在血月期间的丛林）");
+            Replace("Tim's Concoction drop rate", "佩戴蒂姆的秘药时的掉落率");
+            void Replace(string old, string @new)
             {
-                if (condition != null)
-                {
-                    string conditionDescription = condition.GetConditionDescription();
-                    if (!string.IsNullOrWhiteSpace(conditionDescription))
-                    {
-                        list.Add(conditionDescription
-                            .Replace("Hardmode Eternity Mode drop rate", "在困难模式下的永恒模式下的的掉落率")
-                            .Replace("Pre-Hardmode Eternity Mode drop rate", "在困难模式之前的永恒模式下的掉落率")
-                            .Replace("This is a Master/Eternity Mode drop rate", "这是大师或永恒模式下的掉落率")
-                            .Replace("Non-Eternity Mode drop rate", "在非永恒模式下的掉落率")
-                            .Replace("Eternity Mode drop rate", "在永恒模式下的掉落率")
-                            .Replace("捐赠者掉落 在永恒模式", "在永恒模式下作为捐赠者物品的掉落率")
-                            .Replace("捐赠者掉落", "作为捐赠者物品的掉落率")
-                            .Replace("在血月的丛林", "（在血月期间的丛林）")
-                            .Replace("Tim's Concoction drop rate", "佩戴蒂姆的秘药时的掉落率"));
-                    }
-                }
+                c.Emit(OpCodes.Ldstr, old);
+                c.Emit(OpCodes.Ldstr, @new);
+                c.Emit(OpCodes.Callvirt, typeof(string).GetMethod("Replace", new[] { typeof(string), typeof(string) }));
             }
-
-            FieldInfo fi = typeof(Terraria.GameContent.UI.Elements.UIBestiaryInfoItemLine).GetField("_infoDisplayItem", BindingFlags.Instance | BindingFlags.NonPublic);
-            if (fi?.GetValue(self) is Item infoDisplayItem)
-                infoDisplayItem.BestiaryNotes = string.Join("\n", list);
         }
 
         public override void Unload()
         {
-            On.Terraria.GameContent.UI.Elements.UIBestiaryInfoItemLine.SetBestiaryNotesOnItemCache -= UIBestiaryInfoItemLine_SetBestiaryNotesOnItemCache;
+            IL.Terraria.GameContent.UI.Elements.UIBestiaryInfoItemLine.SetBestiaryNotesOnItemCache += UIBestiaryInfoItemLine_SetBestiaryNotesOnItemCache;
         }
     }
 }
