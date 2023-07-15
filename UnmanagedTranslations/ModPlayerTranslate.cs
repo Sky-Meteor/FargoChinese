7dc1372e96094b07d5792a725e2f729ffe987689
FallinLeaf <3324888485@qq.com>
2023-07-15 21:27:09 +0800

    改了一天终于改完了

--- UnmanagedTranslations/ModPlayerTranslate.cs
+++ UnmanagedTranslations/ModPlayerTranslate.cs
@@ -11,6 +11,10 @@ namespace FargoChinese.UnmanagedTranslations
             string deathText = damageSource.GetDeathText(Player.name).ToString();
             if (deathText.Contains(" was destroyed by their own magic."))
                 damageSource = PlayerDeathReason.ByCustomReason($"{Player.name}被自己的魔法摧毁了。");
+            if (deathText.Contains("感到了心碎。"))
+                damageSource = PlayerDeathReason.ByCustomReason($"{Player.name}被假心欺骗了。");
+            if (deathText.Contains(" was pricked by a Cactus."))
+                damageSource = PlayerDeathReason.ByCustomReason($"{Player.name}被仙人掌扎死了。");
             return base.PreHurt(pvp, quiet, ref damage, ref hitDirection, ref crit, ref customDamage, ref playSound, ref genGore, ref damageSource, ref cooldownCounter);
         }
     }
