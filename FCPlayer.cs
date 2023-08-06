1d02fb3acad4423e5c3ebded3cafe417df8660e8
Furgo <furgos@qq.com>
2023-08-06 15:23:30 +0800

    有点奇怪，下次更新改一下

--- FCPlayer.cs
+++ FCPlayer.cs
@@ -15,7 +15,7 @@ namespace FargoChinese
         public override void OnEnterWorld()
         {
             Color c = Color.Orange;
-            Main.NewText("- Fargo突变 V 3.0 更新 -", c);
+            Main.NewText("- Fargo突变 V 3.0.0 更新 -", c);
             string text = @"-添加了[i:Fargowiltas/PurityTotem] 纯净图腾
 -更新到tModLoader 1.4.4";
             string[] textLines = text.Split("-");
